0b41f95e4dd3bf2ccf9ff2fbbf20478685477f95
ibrahim <saffetk502@gmail.com>
2021-06-06 15:26:16 +0200

    Konflikt olusturma

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("first commit");
-		System.out.println("second commit");
+		System.out.println("second commit lokal repository'dan");
 		System.out.println("GitHub'tan selamlar");
 		
 	}
